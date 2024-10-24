4aae74e3defe02a5996734b109ff03a8285f67b0
gerald <geraldbaalham@live.co.uk>
2024-10-24 11:04:55 +0100

    commit

---
+++ src/routers/AdminRoute.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+
+import { Navigate, Outlet } from "react-router-dom";
+
+const AdminRoute = ({ children }) => {
+  const token = localStorage.getItem("token");
+  if (!token) {
+    return <Navigate to="/admin" />;
+  }
+  return children ? children : <Outlet />;
+};
+
+export default AdminRoute;
---
+++ src/utils/baseURL.js
@@ -0,0 +1,5 @@
+const getBaseUrl = () => {
+  return "http://localhost:3005";
+};
+
+export default getBaseUrl;
